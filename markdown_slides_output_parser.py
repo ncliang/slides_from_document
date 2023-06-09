d08d5df049894fa04daf6320ab7f881dbbfe354f
Nigel Liang <nigel@nigelliang.com>
2023-06-09 14:14:21 +0800

    MINOR: refactor; update comments; update test

--- markdown_slides_output_parser.py
+++ markdown_slides_output_parser.py
@@ -22,6 +22,30 @@ class MarkdownSlidesOutputParser(BaseOutputParser):
         slides.insert(0, title_slide)
 
     @staticmethod
+    def _handle_header(cur, cur_slide, slides):
+        if not cur_slide:
+            cur_slide = SlideWithSubtitle()
+            cur_slide.subtitle = get_content_only(cur)
+        elif isinstance(cur_slide, SlideWithBulletPoints):
+            cur_slide.title = get_content_only(cur)
+            slides.insert(0, cur_slide)
+            cur_slide = None
+        elif isinstance(cur_slide, SlideWithSubtitle):
+            cur_slide.title = get_content_only(cur)
+            MarkdownSlidesOutputParser._insert_title_slide(slides, cur_slide)
+            cur_slide = None
+        return cur_slide
+
+    @staticmethod
+    def _handle_bullet_point(cur, cur_slide, lines):
+        if not cur_slide:
+            cur_slide = SlideWithBulletPoints()
+        if not isinstance(cur_slide, SlideWithBulletPoints):
+            raise ValueError(f"Error parsing {cur} in {lines}")
+        cur_slide.bullet_points.insert(0, BulletPoint.from_bullet_point_line(cur))
+        return cur_slide
+
+    @staticmethod
     def _is_bullet_point(line):
         return line and not line.startswith("#")
 
@@ -32,9 +56,11 @@ class MarkdownSlidesOutputParser(BaseOutputParser):
 
         cur_slide = None
 
-        # 預期除了第一張投影片有可能是標題跟副標以外其他投影片一律是標題跟bullet point要點式的投影片
-        # Markdown裡面'-'、'*'跟數字開頭的都是bullet point。其餘'#'開頭的都是標題或副標
-        # 演算法是從後面往前看。看到bullet point就加到目前的
+        # 處理回傳的markdown字串。把markdown轉成`markdown_slide.py`裡的物件
+        # 演算法是從最後一行往前看。任何'#'開頭的都當成投影片標頭處理。其他所有內容都當bullet point要點處理
+        # 所有文字格式都會被忽略。內嵌的圖片會忽略。超連結會忽略。
+        # 只支援產生含有標題副標題跟含有標題跟bullet point要點的投影片
+        # bullet point可以有階層。含有數字的列表會被轉成無數字的列表
         while lines:
             cur = lines.pop(-1)
             try:
@@ -56,25 +82,4 @@ class MarkdownSlidesOutputParser(BaseOutputParser):
             self._insert_title_slide(slides, cur_slide)
         return slides
 
-    def _handle_header(self, cur, cur_slide, slides):
-        if not cur_slide:
-            cur_slide = SlideWithSubtitle()
-            cur_slide.subtitle = get_content_only(cur)
-        elif isinstance(cur_slide, SlideWithBulletPoints):
-            cur_slide.title = get_content_only(cur)
-            slides.insert(0, cur_slide)
-            cur_slide = None
-        elif isinstance(cur_slide, SlideWithSubtitle):
-            cur_slide.title = get_content_only(cur)
-            self._insert_title_slide(slides, cur_slide)
-            cur_slide = None
-        return cur_slide
-
-    def _handle_bullet_point(self, cur, cur_slide, lines):
-        if not cur_slide:
-            cur_slide = SlideWithBulletPoints()
-        if not isinstance(cur_slide, SlideWithBulletPoints):
-            raise ValueError(f"Error parsing {cur} in {lines}")
-        cur_slide.bullet_points.insert(0, BulletPoint.from_bullet_point_line(cur))
-        return cur_slide
 
